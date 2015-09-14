0db9e1c751f762768ad951ac119db84672a346a2
Andres Roget <andres.roget@lightspeedpos.com>
2015-09-14 05:48:09 -0400

    more

--- web.js
+++ web.js
@@ -1,7 +1,27 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
+var express        = require('express');
+var bodyParser = require('body-parser');
+var app            = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3000);
+// set our port
+var port = process.env.PORT || 3000;
+
+// set the static files location
+app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+// require('./routes/routes')(app); // configure our routes
+
+// startup our app at http://localhost:3000
+app.listen(port);
+
+
+// expose app
+exports = module.exports = app;
